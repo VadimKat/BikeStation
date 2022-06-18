5583e7836c2ed28b297b179590b86579021b3267
Vadim Katenin <vadimkatenin1@gmail.com>
2022-06-18 14:12:08 +0300

    Initial Commit

---
+++ BikeStationAssignment/BikeStationAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeStationAssignmentApp.swift
+//  BikeStationAssignment
+//
+//  Created by Vadim Katenin on 18.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct BikeStationAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
